a0bdb02e5c655cac702fa99b6f96858e2e1d13b8
Arely7 <jrp678179@gmail.com>
2026-02-26 14:53:53 -0600

    Cambio de pantalla

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -7,4 +7,16 @@ class LoginController:
         self.window.btn_login.clicked.connect(self.handle_login)
 
     def handle_login(self):
+        username = self.window.txt_username.text()
+        password = self.window.txt_password.text()
+        if username == "admin" and password == "123":
+            self.window.login_successfull.emit()
+            print("Login Correcto✔️")
+        else:
+            #print("Login Incorrecto✖️")
+            QtWidgets.QMessageBox.warning(
+                self.window,
+                "Abarrotes TEC - ERROR",
+                "Login Incorrecto✔️"
+            )
         print("Login button clicked😎😊✌️👍")
